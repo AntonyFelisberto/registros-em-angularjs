a03e35629aac8889cb76ddb49cdf2599ab31b3bb
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-02-04 17:53:14 -0300

    adicionando rotas na aplicacao

---
+++ projeto/js/controllers/registration.js
@@ -0,0 +1,4 @@
+//VOCE SÓ PRECISA CRIAR O NG-APP UMA VEZ, APÓS ISSO VOCE PODE USAR A MESMA VARIAVEL, A MENOS QUE QUEIRA TER MAIS DE UM PARA AÇÕES EM PAGINAS DIFERENTES
+app.controller("RegistrationController",[$scope,function($scope){
+    $scope.mensagem = "Bem Vindo";
+}])
