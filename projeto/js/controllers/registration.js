936f7424cd1986a2d55b7b72c7b2c184b20e8cf2
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-02-04 18:49:49 -0300

    corrigindo scope sem aspas

--- projeto/js/controllers/registration.js
+++ projeto/js/controllers/registration.js
@@ -1,4 +1,4 @@
 //VOCE SÓ PRECISA CRIAR O NG-APP UMA VEZ, APÓS ISSO VOCE PODE USAR A MESMA VARIAVEL, A MENOS QUE QUEIRA TER MAIS DE UM PARA AÇÕES EM PAGINAS DIFERENTES
-app.controller("RegistrationController",[$scope,function($scope){
+app.controller("RegistrationController",["$scope",function($scope){
     $scope.mensagem = "Bem Vindo";
 }])
