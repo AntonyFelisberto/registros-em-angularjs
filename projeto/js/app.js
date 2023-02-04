b32a131ddf7c0f8499d083d5529257d450746a11
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-02-04 17:29:47 -0300

    adicionando rotas no angular

--- projeto/js/app.js
+++ projeto/js/app.js
@@ -1,5 +1,20 @@
-var app = angular.module("myApp",[]);
+var app = angular.module("myApp",['ng-route']);
 
-app.controller("myController",["$scope",function($scope){
-    $scope.mensagem = "BEM VINDO";
+app.config(['$routeProvider',function($routeProvider){
+    $routeProvider.
+        when('/login',{
+            temmplateUrl:"views/login.html",
+            controller:"RegistrationController"
+        }).
+        when('/register',{
+            temmplateUrl:"views/register.html",
+            controller:"RegistrationController"
+        }).
+        when('/sucess',{
+            temmplateUrl:"views/sucess.html",
+            controller:"SucessController"
+        }).
+        otherwise({
+            redirecTo:'/login'
+        })
 }])
