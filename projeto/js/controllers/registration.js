5633f08fb89dce83510fc19f9ec82be24f3abf11
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-02-20 15:25:32 -0300

    fim do projeto

--- projeto/js/controllers/registration.js
+++ projeto/js/controllers/registration.js
@@ -1,4 +1,11 @@
 //VOCE SÓ PRECISA CRIAR O NG-APP UMA VEZ, APÓS ISSO VOCE PODE USAR A MESMA VARIAVEL, A MENOS QUE QUEIRA TER MAIS DE UM PARA AÇÕES EM PAGINAS DIFERENTES
 app.controller("RegistrationController",["$scope",function($scope){
-    $scope.mensagem = "Bem Vindo";
+   
+    $scope.login = function(){
+        $scope.mensagem = "Welcome " + $scope.user.email;
+    }
+
+    $scope.register = function(){
+        $scope.mensagem = "Welcome " + $scope.user.firstname;
+    }
 }])
